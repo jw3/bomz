2b4d1a788079ce1843bd02e7941b9103e7f5e055
John Wass <jwass3@gmail.com>
2020-04-15 21:54:51 -0400

    load sprite sheets from config

---
+++ src/main/scala/bz/apps/SpriteLibViewer.scala
@@ -0,0 +1,37 @@
+package bz.apps
+
+import java.awt.{Color, Dimension}
+
+import bz.sprites
+import com.typesafe.config.ConfigFactory
+import javax.swing.{BorderFactory, ImageIcon}
+
+import scala.swing.{FlowPanel, Label, MainFrame}
+
+object SpriteLibViewer extends App {
+  val config = ConfigFactory.load("sheet.conf").getConfig("sheet")
+  val lib = sprites.library.init(config)
+
+  new MainFrame {
+    title = "bomz"
+
+    contents = new FlowPanel {
+      contents ++= lib.zipWithIndex.flatMap {
+        case (b, i) =>
+          new Label(b.id) +:
+            b.tiles.map { bi =>
+              new Label {
+                icon = new ImageIcon(bi) {
+                  border = BorderFactory.createLineBorder(Color.black)
+                }
+              }
+            }
+      }
+    }
+
+    preferredSize = new Dimension(560, 690)
+    pack()
+    centerOnScreen()
+    open()
+  }
+}
